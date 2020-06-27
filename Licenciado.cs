72240940ca647cdcd6364546ee38645eef310cb2
andy de la cruz <drag.andy@outlook.com>
2020-06-27 12:56:29 -0500

    modificaion

---
+++ Licenciado.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesPOO
+{
+    public class Licenciado
+    {
+        private int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public int AñosProfesión { get; set; }
+
+        Carrera carrera = new Carrera
+        {
+            Nombre = "Desarrollo de Sotware"
+        };
+
+        Curso curso = new Curso
+        {
+            Nombre = "Curso 46",
+
+            PresidenteCurso = "Andy de la cru Cruz",
+        };
+
+        public string Hablar()
+        {
+            return $"Hola  su nombre es  {Nombre} tiene    {Edad}  años   y su  experiencia laboral es  de {AñosProfesión} ";
+        }
+
+        public string CarreraQueda()
+        {
+            return $"Es  asignado  para  dar la carrera de :{carrera.Nombre}";
+        }
+
+        public string AsignadoCurso()
+        {
+
+            return $" Su curso  asignado es {curso.Nombre} y  el Presidente de aquel curso es:  {curso.PresidenteCurso}";
+        }
+
+        public string SegundoAsignado(string nombre)
+        {
+            return $"{nombre} Es  el Vicepresidente";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,69 @@ namespace ClasesPOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo..como estan");
-        
+            Console.WriteLine("HOLA A TODOS SEAN BIENVENIDOS");
+
+            Console.WriteLine("*************");
+            Console.ReadKey();
+
+            Estudiantes estudiante = new Estudiantes
+            {
+                Nombre = "Kevin Cabrera ",
+                Edad = 26,
+                Peso = 1.39,
+                Ciudad = "Quito",
+            };
+
+            Console.WriteLine(estudiante.Presentarse());
+
+            Console.WriteLine(estudiante.CursoAlQueIntegra());
+
+            Console.WriteLine(estudiante.MateriaAsignada());
+
+            Console.WriteLine("**********");
+            Console.ReadKey();
+
+            Licenciado licenciado = new Licenciado
+            {
+                Nombre = "Carlos Pozo",
+                AñosProfesión = 3,
+                Edad = 45,
+            };
+
+            Console.WriteLine(licenciado.Hablar());
+
+            Console.WriteLine(licenciado.CarreraQueda());
+
+            Console.WriteLine(licenciado.AsignadoCurso());
+
+            Console.WriteLine(licenciado.SegundoAsignado(estudiante.Nombre));
+
+            Console.WriteLine("***************");
+            Console.ReadKey();
+
+            Carrera Carrera = new Carrera
+            {
+                Nombre = "Desarrollo de sofware",
+                Materia = "Base de datos ",
+                Horario = "5-9 de la noche"
+            };
+
+            Console.WriteLine(Carrera.MostrarCarrera());
+
+            Curso curso = new Curso
+            {
+                Nombre = "Curso 100",
+                PresidenteCurso = "Israel Yanez"
+            };
+
+            Console.WriteLine(curso.MostrarCurso());
+
+
+
+
         }
+
     }
+
+
 }
